fb808b7542591f255db353076584747dedcc8fac
User 1 <User 1@DESKTOP-OGKF8HP>
2022-10-10 22:42:54 +0300

    Add project files.

---
+++ ex 1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       Questions examQuestion;
+
+        public MainWindow()
+        {
+            examQuestion.questionCount = 0;
+            InitializeComponent(); 
+            switch (examQuestion.questionCount)
+            {
+                case 0: examQuestion.Question = "Q1";
+                    examQuestion.Answers = new string[] {"a1","a2","a3","a4" };
+                    examQuestion.CorrectAnswer = "a3"; examQuestion.choiceShuffle();
+                    radioButtonsFillUp(); questionLabel.Content = examQuestion.Question; break;
+                case 1:
+                    examQuestion.Question = "Q2";
+                    examQuestion.Answers = new string[] { "a1", "a2", "a3", "a4" };
+                    examQuestion.CorrectAnswer = "a1"; examQuestion.choiceShuffle();
+                    radioButtonsFillUp(); questionLabel.Content = examQuestion.Question; break;
+                case 2:
+                    examQuestion.Question = "Q3";
+                    examQuestion.Answers = new string[] { "a1", "a2", "a3", "a4" };
+                    examQuestion.CorrectAnswer = "a1"; examQuestion.choiceShuffle();
+                    radioButtonsFillUp(); questionLabel.Content = examQuestion.Question; break;
+                case 3:
+                    question1RadioButton.Visibility = Visibility.Hidden;
+                    question2RadioButton.Visibility = Visibility.Hidden;
+                    question3RadioButton.Visibility = Visibility.Hidden;
+                    question4RadioButton.Visibility = Visibility.Hidden;
+                    questionLabel.Visibility = Visibility.Hidden; break;
+
+            }
+            
+        }
+
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            examQuestion.questionCount++;
+        }
+
+        private void radioButtonsFillUp()
+        {
+            question1RadioButton.Content = examQuestion.Answers[0];
+            question2RadioButton.Content = examQuestion.Answers[1];
+            question3RadioButton.Content = examQuestion.Answers[2];
+            question4RadioButton.Content = examQuestion.Answers[3];
+        }
+    }
+}
---
+++ ex 1/Questions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_1
+{
+    // ={"BENG100", "COMP213","COMM123","PSYC200"};
+    internal class Questions
+    {
+        public const int SIZE = 3;
+        public const int NUMBEROFQUESTIONS = 4;
+        public int questionCount = 0;
+        public int resultsScore = 0;
+        public Questions(string question, string[] answers, string correctAnswer)
+        {
+            Question = question;
+            Answers = answers;
+            CorrectAnswer = correctAnswer;
+        }
+        public string? Question { get; set; }
+        public string[]? Answers { get; set; }
+        public string? CorrectAnswer { get; set; }
+
+        Random rnd = new Random();
+
+        public void choiceShuffle()
+        {
+            Answers = Answers.OrderBy(x => rnd.Next()).ToArray();
+        }
+
+    }
+}
+
